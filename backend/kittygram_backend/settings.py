2f74e3ce12107505718f3838b7c1310ece58a851
Kirill Bovin <skrill96@e1.ru>
2023-07-21 21:08:55 +0500

    3

--- backend/kittygram_backend/settings.py
+++ backend/kittygram_backend/settings.py
@@ -8,7 +8,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 
 DEBUG = os.getenv('DEBUG', False) == 'True'
 
-ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS').split(',')
+ALLOWED_HOSTS = ['84.201.165.14', '127.0.0.1', 'localhost', 'kitti196.hopto.org']
 
 
 INSTALLED_APPS = [
